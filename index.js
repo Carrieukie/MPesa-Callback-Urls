720aae6cd921fe70dbab274b3afff92465b77c8a
Carrieukie <kariuki931@gmail.com>
2021-07-30 13:16:05 +0300

    Adding node js db

--- index.js
+++ index.js
@@ -6,37 +6,52 @@ app.use(bodyParser.json())
 
 const port = process.env.port || 3000
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
+
+
 app.get("/" , (req, res) => res.send("Hello world"))
 
 app.post('/confirmation', (req, res) => {
     console.log('....................... confirmation .............')
-    console.log(JSON.stringify(req.body))
+    var body = JSON.stringify(req.body)
 
-    var sqlite3 = require('sqlite3').verbose();
-    var db = new sqlite3.Database(':memory:');
+    console.log(body)
 
-    db.serialize(function() {
-    db.run("CREATE TABLE lorem (info TEXT)");
 
-    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    stmt.run("Ipsum " + JSON.stringify(req.body));
-    stmt.finalize();
+    if(body.length > 3){
+        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        stmt.run("Ipsum " + body);
+        stmt.finalize();
+    }
 
-    db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-         console.log(row.id + ": " + row.info);
-         res.send(row.id + ": " + row.info)
+
+    db.all("SELECT * FROM lorem", function(err, row) {
+         console.log(err)
+         console.log(row)
+         res.send(row)
     });
 });
 
-db.close();
+// db.close();
 
 })
 
 app.post('/validation', (req, resp) => {
-    console.log('....................... validation .............')
+    console.log('....................... validation .......................')
     console.log(req.body)
 })
 
 app.listen(process.env.PORT || 3000 , () => {
-    console.log("App listening on port" + port)
+    console.log("App listening on port " + port)
 })
+
+
+
+
+
+
+
