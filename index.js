54edda5a922db1c4fece4fbdd8d56e9629666eaa
Carrieukie <kariuki931@gmail.com>
2021-07-30 14:06:52 +0300

    Validation URL logic

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
 
 db.serialize(function() {
-db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS confirmation (info TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS validation (info TEXT)");
 
 
 app.get("/" , (req, res) => res.send("Hello world"))
@@ -23,27 +24,43 @@ app.post('/confirmation', (req, res) => {
 
 
     if(body.length > 3){
-        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        var stmt = db.prepare("INSERT INTO confirmation VALUES (?)");
         stmt.run("Ipsum " + body);
         stmt.finalize();
     }
 
 
-    db.all("SELECT * FROM lorem", function(err, row) {
+    db.all("SELECT * FROM confirmation", function(err, row) {
          console.log(err)
          console.log(row)
          res.send(row)
     });
 });
 
-// db.close();
 
 })
 
-app.post('/validation', (req, resp) => {
+app.post('/validation', (req, res) => {
     console.log('....................... validation .......................')
-    console.log(req.body)
-})
+
+    var body = JSON.stringify(req.body)
+    console.log(body)
+
+
+    if(body.length > 3){
+        var stmt = db.prepare("INSERT INTO validation VALUES (?)");
+        stmt.run("Ipsum " + body);
+        stmt.finalize();
+    }
+
+
+    db.all("SELECT * FROM validation", function(err, row) {
+         console.log(err)
+         console.log(row)
+         res.send(row)
+    });
+});
+
 
 app.listen(process.env.PORT || 3000 , () => {
     console.log("App listening on port " + port)
