9e1d36d7a254eee179f4c664a317139a84aab91b
Carrieukie <kariuki931@gmail.com>
2021-08-04 03:21:02 +0300

    Adding units

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ var db = new sqlite3.Database(':memory:');
 db.serialize(function() {
 db.run("CREATE TABLE IF NOT EXISTS confirmation (info TEXT)");
 db.run("CREATE TABLE IF NOT EXISTS validation (info TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS b2c_timeout_url (info TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS b2c_result_url (info TEXT)");
+db.run("CREATE TABLE IF NOT EXISTS stk_callback (info TEXT)");
 db.run("CREATE TABLE IF NOT EXISTS status (info TEXT)");
+
 });
 
 app.get("/" , (req, res) => res.send("On this api send an empty POST request to /confirmation for confirmation  \n /validation for validation \n /status for transaction query status"))
@@ -61,6 +65,90 @@ app.post('/validate', (req, res) => {
          console.log(err)
          console.log(row)
          res.send(row)
+    });
+});
+
+app.post('/b2c_timeout_url', (req, res) => {
+
+    console.log('....................... status .......................')
+
+    //Get the request body
+    var body = JSON.stringify(req.body)
+    console.log(body)
+
+    //This is to avoid empty columns in the database
+    //Insert the body into database if there's any
+    if(body.length > 3){
+        var stmt = db.prepare("INSERT INTO b2c_timeout_url VALUES (?)");
+        stmt.run("Status " + body);
+        stmt.finalize();
+    }
+
+    //Fetch everything from the database from the confrimation table
+    db.all("SELECT * FROM b2c_timeout_url", function(err, row) {
+    
+         if(err){
+             res.send(err)
+         }else{
+            res.send(row)
+         }
+
+    });
+});
+
+app.post('/b2c_result_url', (req, res) => {
+
+    console.log('....................... b2c_result_url .......................')
+
+    //Get the request body
+    var body = JSON.stringify(req.body)
+    console.log(body)
+
+    //This is to avoid empty columns in the database
+    //Insert the body into database if there's any
+    if(body.length > 3){
+        var stmt = db.prepare("INSERT INTO b2c_result_url VALUES (?)");
+        stmt.run("b2c_result" + body);
+        stmt.finalize();
+    }
+
+    //Fetch everything from the database from the confrimation table
+    db.all("SELECT * FROM b2c_result_url", function(err, row) {
+    
+         if(err){
+             res.send(err)
+         }else{
+            res.send(row)
+         }
+
+    });
+});
+
+app.post('/stk_callback', (req, res) => {
+
+    console.log('....................... stk_callback .......................')
+
+    //Get the request body
+    var body = JSON.stringify(req.body)
+    console.log(body)
+
+    //This is to avoid empty columns in the database
+    //Insert the body into database if there's any
+    if(body.length > 3){
+        var stmt = db.prepare("INSERT INTO stk_callback VALUES (?)");
+        stmt.run("stk_callback " + body);
+        stmt.finalize();
+    }
+
+    //Fetch everything from the database from the confrimation table
+    db.all("SELECT * FROM stk_callback", function(err, row) {
+    
+         if(err){
+             res.send(err)
+         }else{
+            res.send(row)
+         }
+
     });
 });
 
